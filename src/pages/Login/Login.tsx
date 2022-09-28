0f738474817230e42be1ba8a8443c575f6c34a4e
Marcos Campos <marxmyers@icloud.com>
2022-09-28 04:16:46 -0300

    feat: add header to login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,11 @@
 import { LoginForm } from 'components/Forms';
+import { Header } from 'components/Header';
 import './styles/Login.styles.css';
 
 export function Login() {
   return (
     <div>
+      <Header />
       <LoginForm />
     </div>
   );
